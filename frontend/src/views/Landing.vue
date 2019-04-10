a0c5b6653271746163b089c8317f49b9d5122864
Tia Smith <tia.smith@gmail.com>
2019-04-10 08:47:39 -0400

    Added login and guest options on landing

--- frontend/src/views/Landing.vue
+++ frontend/src/views/Landing.vue
@@ -1,10 +1,17 @@
 <template>
-    <h1>Welcome</h1>
+    <div id="landing">
+        <h1>Welcome</h1>
+        <h2><router-link to="/login">Login</router-link></h2>
+        <h2><router-link to="/profile">Continue as Guest</router-link></h2>
+    </div>
 </template>
 <script>
 </script>
 <style>
 body {
     text-align: center;
+}
+router-link {
+    text-decoration: none;
 }
 </style>
