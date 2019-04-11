669c25d816f98bbf37091d7c6c08c0e441ceadd9
Trevor Werner <trevorwerner@hotmail.com>
2019-04-11 11:21:50 -0400

    Created models for db tables, along with dao's for Food table

---
+++ backend/SampleApi/DAL/FoodSqlDAO.cs
@@ -0,0 +1,41 @@
+﻿using SampleApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleApi.DAL
+{
+    public class FoodSqlDAO : IFoodDAO
+    {
+        public string connectionString;
+
+        public FoodSqlDAO(string databaseConnectionString)
+        {
+            connectionString = databaseConnectionString;
+        }
+
+        /// <summary>
+        /// This method provides the ability to add a food item to the db.
+        /// </summary>
+        /// <param name="food"></param>
+        public void AddFoodItem(Food food)
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+    }
+}
---
+++ backend/SampleApi/Models/Profile.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleApi.Models
+{
+    public class Profile
+    {
+        public int UserId { get; set; }
+
+        public decimal CurrentWeight { get; set; }
+
+        public decimal GoalWeight {get;set;}
+
+        public int age { get; set; }
+
+        public int height { get; set; }
+
+        public string ActivityLevel { get; set; }
+
+    }
+}
