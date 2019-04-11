15ee1a89c5197914d9a56ef4ef7d5c9bb17a19f6
John Freund <jfreund91@gmail.com>
2019-04-11 08:04:08 -0400

    Added food class to model the food items in back end

---
+++ backend/SampleApi/Models/Food.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleApi.Models
+{
+    /// <summary>
+    /// Models a food item.
+    /// </summary>
+    public class Food
+    {
+        /// <summary>
+        /// The name of the food
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Represents the amount of calories in a food item.
+        /// </summary>
+        public decimal Calories { get; set; }
+
+        /// <summary>
+        /// Represents the amout of fat(in grams) in a food item item.
+        /// </summary>
+        public decimal Fat { get; set; }
+
+        /// <summary>
+        /// Represents the amount of carbohydrates(in grams) in a food item.
+        /// </summary>
+        public decimal Carbohydrates { get; set; }
+        /// <summary>
+        /// Represents the amount of protein(in grams) in a food item.
+        /// </summary>
+        public decimal Protein { get; set; }
+
+
+    }
+}
