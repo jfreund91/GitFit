c8f2f3d7e6a7dd5aec67426897db4f5e408e7dd5
Trevor Werner <trevorwerner@hotmail.com>
2019-04-11 12:30:45 -0400

    Added CreateProfile method for profileDAO

--- backend/SampleApi/DAL/ProfileSqlDAO.cs
+++ backend/SampleApi/DAL/ProfileSqlDAO.cs
@@ -1,11 +1,49 @@
-﻿using System;
+﻿using SampleApi.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace SampleApi.DAL
 {
-    public class ProfileSqlDAO
+    public class ProfileSqlDAO : IProfileDAO
     {
+        private string connectionString;
+
+        public ProfileSqlDAO(string databaseConnectionString)
+        {
+            connectionString = databaseConnectionString;
+        }
+
+        public void CreateProfile(Profile profile)
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    // Open the connection
+                    conn.Open();
+
+                    SqlCommand cmd = new SqlCommand("INSERT INTO user_profiles VALUES (@name, " +
+                        "@currentWeight, @goalWeight, @age, @height, @activityLevel, @gender);", conn);
+                    cmd.Parameters.AddWithValue("@name", profile.Name);
+                    cmd.Parameters.AddWithValue("@currentWeight", profile.CurrentWeight);
+                    cmd.Parameters.AddWithValue("goalWeight", profile.GoalWeight);
+                    cmd.Parameters.AddWithValue("@age", profile.Age);
+                    cmd.Parameters.AddWithValue("@height", profile.Height);
+                    cmd.Parameters.AddWithValue("@activityLevel", profile.ActivityLevel);
+                    cmd.Parameters.AddWithValue("@gender", profile.Gender);
+
+                    cmd.ExecuteScalar();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
     }
 }
