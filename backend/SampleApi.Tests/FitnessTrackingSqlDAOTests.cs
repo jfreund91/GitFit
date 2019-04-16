25ab8503b61331ab63a456329e1dc8e37377238c
Trevor Werner <trevorwerner@hotmail.com>
2019-04-16 15:40:19 -0400

    added RemoveFoodItem unit test

--- backend/SampleApi.Tests/FitnessTrackingSqlDAOTests.cs
+++ backend/SampleApi.Tests/FitnessTrackingSqlDAOTests.cs
@@ -73,13 +73,47 @@ namespace SampleApi.Tests
             User user = new User() { Username = "Name", Password = "Password", Salt = "salt", Role = "role" };
             user.Id = (userDAO.CreateUser(user)).Id;
 
-            Profile profile  = new Profile() { UserId=user.Id, Name = "Name", CurrentWeight = 100, GoalWeight=200, BirthDate=DateTime.Today, Feet=6, Inches =9, ActivityLevel="moderate", Gender='F'};
+            Profile profile  = new Profile() { UserId=user.Id, Name = "Name", CurrentWeight = 100, GoalWeight=200, BirthDate=DateTime.Today, Feet=6, Inches =9, ActivityLevel="moderate", Gender='F', Timeline="Maintain"};
             profileDAO.CreateProfile(profile);
             int actual = this.GetRowCount("user_profiles");
 
             //ASSERT
             Assert.AreEqual(1, actual);
         }
+
+        [TestMethod]
+        // This unit test is designed to verify the AddFoodItem method is adding a new row on the user db table
+        public void RemoveFoodItem_ShouldReturnZeroRows()
+        {
+            //ARRANGE
+            FoodSqlDAO foodItemDAO = new FoodSqlDAO("Server=.\\SQLEXPRESS;Database=DemoDB;Trusted_Connection=True;");
+            UserSqlDAO userDAO = new UserSqlDAO("Server=.\\SQLEXPRESS;Database=DemoDB;Trusted_Connection=True;");
+
+
+            //ACT
+            User user = new User() { Username = "Name", Password = "Password", Salt = "salt", Role = "role" };
+            userDAO.CreateUser(user);
+            Food item = new Food()
+            {
+                Calories = 1000,
+                Name = "Salami",
+                Fat = 20,
+                Carbs = 10,
+                Protein = 30,
+                MealType = "Lunch",
+                Servings = 3,
+                Date = DateTime.Now,
+                ndbno = 200
+            };
+
+            foodItemDAO.AddFoodItem(user.Id, item);
+            foodItemDAO.RemoveFoodItem(item.EntryId);
+            int actual = this.GetRowCount("food_entries");
+
+            //ASSERT
+            Assert.AreEqual(0, actual);
+        }
+
 
     }
 }
